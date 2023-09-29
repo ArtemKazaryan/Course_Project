21f206696c722ec5d9ba84d71cbd62f701481370
Artem Kazaryan <mirony4@yandex.ru>
2023-09-29 18:34:09 +0300

    check point

--- Personal_money_transactions/transaction/views.py
+++ Personal_money_transactions/transaction/views.py
@@ -60,14 +60,17 @@ def recordedtransactions(request):
     protransactions = ProfitableTransaction.objects.all()
     exptransactions = ExpenditureTransaction.objects.all()
 
+    # Получение списков из qweryset'ов
     valuespro_list = protransactions.values()
     valuesexp_list = exptransactions.values()
 
+    # Задаём переменный общих размеров доходов и расходов, а также счётчики транзакций
     sumpro = 0
     sumexp = 0
     countpro = 0
     countexp = 0
 
+    # Итерируемся по спискам
     for item in valuespro_list:
         valuepro = item['amount']
         sumpro += valuepro
@@ -78,37 +81,47 @@ def recordedtransactions(request):
         sumexp += valueexp
         countexp += 1
 
+    # Вычисляем баланс нашей учётной базы
     total_balance = sumpro - sumexp
 
+    # Получаем минимальную дату из БД по доходам
     oldest_date_pro = ProfitableTransaction.objects.aggregate(Min('date'))['date__min']
     if not oldest_date_pro:
         return 0
     # Разница между текущей датой и самой старой датой
     delta_date_pro = (date.today() - oldest_date_pro).days + 1
 
+    # Получаем минимальную дату из БД по расходам
     oldest_date_exp = ExpenditureTransaction.objects.aggregate(Min('date'))['date__min']
     if not oldest_date_exp:
         return 0
     # Разница между текущей датой и самой старой датой
     delta_date_exp = (date.today() - oldest_date_exp).days + 1
 
+    # Находим общий срок ведения учёта
     delta_days = [delta_date_pro, delta_date_exp]
     max_delta_days = max(delta_days)
 
+    # Находим самую первую дату учёта
     if delta_date_pro >= delta_date_exp:
         oldest_of_oldest_dates = oldest_date_pro
     else:
         oldest_of_oldest_dates = oldest_date_exp
 
+    # Вычисляем скорости доходов и затрат
     total_revenue_rate = round(sumpro / max_delta_days, 2)
     total_expense_rate = round(sumexp / max_delta_days, 2)
 
+    # Вычисляем скорость прибыли
     margin_total_rate = total_revenue_rate - total_expense_rate
 
+    # Получаем текущую дату
     today = date.today()
 
+    # Эта переменная для пунктира
     multidash = '- ' * 117
 
+    # Формируем контекст вывода на страницу
     context = {'protransactions': protransactions, 'exptransactions': exptransactions,
                'sumpro': sumpro, 'sumexp': sumexp, 'countpro': countpro, 'countexp': countexp,
                'total_revenue_rate': total_revenue_rate, 'total_expense_rate': total_expense_rate,
